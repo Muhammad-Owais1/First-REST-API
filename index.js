979423d9cbb9d7a75d9a856d5e7028f877f15692
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2024-06-09 19:38:15 +0500

    Made /users route work.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const users = require('./MOCK_DATA.json')
+
 const app = express()
 const PORT = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+app.get('/users', (req, res) => {
+    return res.json(users)
 })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
